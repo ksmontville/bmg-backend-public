1aac5c1a026ef015dc2acba65ca3f5d34fd5193b
Kyle <ksmontville@gmail.com>
2022-12-02 12:01:29 -0500

    Add link field to banner component.

--- banner/models.py
+++ banner/models.py
@@ -12,6 +12,7 @@ class Banner(Page):
 
     banner_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
     banner_subtext = RichTextField(features=ALL_FEATURES, blank=True, null=True)
+    banner_link = RichTextField(features=ALL_FEATURES, blank=True, null=True)
     banner_image = models.ForeignKey(
         'wagtailimages.Image',
         blank=True,
@@ -23,11 +24,13 @@ class Banner(Page):
     content_panels = Page.content_panels + [
         FieldPanel('banner_text'),
         FieldPanel('banner_subtext'),
+        FieldPanel('banner_link'),
         FieldPanel('banner_image'),
     ]
 
     api_fields = [
         APIField('banner_text'),
         APIField('banner_subtext'),
+        APIField('banner_link'),
         APIField('image', serializer=ImageRenditionField('fill-100x100', source='banner_image')),
     ]
