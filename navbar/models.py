bfc2d50fe0f396373a88bb8b508679e7fc3479fd
Kyle <ksmontville@gmail.com>
2022-12-02 11:19:59 -0500

    Add hero image.

--- navbar/models.py
+++ navbar/models.py
@@ -11,14 +11,14 @@ class Navbar(Page):
     page_description = "Tools for editing the navbar."
 
     nav_text = RichTextField(features=ALL_FEATURES, blank=True)
-    nav_btn = models.ForeignKey(
+    nav_button = models.ForeignKey(
         'wagtailimages.Image',
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    nav_img = models.ForeignKey(
+    nav_image = models.ForeignKey(
         'wagtailimages.Image',
         blank=True,
         null=True,
@@ -28,13 +28,12 @@ class Navbar(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('nav_text'),
-        FieldPanel('nav_btn'),
-        FieldPanel('nav_img'),
+        FieldPanel('nav_button'),
+        FieldPanel('nav_image'),
     ]
 
     api_fields = [
         APIField('nav_text'),
-        APIField('button', serializer=ImageRenditionField('fill-12x12', source='nav_btn')),
-        # APIField('logo', serializer=ImageRenditionField('fill-48x48', source='nav_img')),
-        APIField('nav_img'),
+        APIField('button', serializer=ImageRenditionField('fill-12x12', source='nav_button')),
+        APIField('image', serializer=ImageRenditionField('fill-64x64', source='nav_image')),
     ]
