f3b1a6bfb3d79f441fc033bb96df58c16cf6f888
Kyle <ksmontville@gmail.com>
2022-12-05 18:49:10 -0500

    Redo streamfield api serializer.

--- products/models.py
+++ products/models.py
@@ -6,59 +6,61 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
 from wagtail.images.api.fields import ImageRenditionField
 from home.models import ALL_FEATURES
-from wagtail.images.blocks import ImageChooserBlock as DefaultImageChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
 
 
-class ImageChooserBlock(DefaultImageChooserBlock):
-    def get_api_representation(self, value, context=None):
-        if value:
-            return {
-                'id': value.id,
-                'title': value.title,
-                'large': value.get_rendition('width-1000').attrs_dict,
-                'thumbnail': value.get_rendition('fill-128x128').attrs_dict,
-                'url': value.get_rendition('fill-128x128').url,
-                'alt': value.get_rendition('fill-128x128').alt
-            }
+# class ImageChooserBlock(DefaultImageChooserBlock):
+#     def get_api_representation(self, value, context=None):
+#         if value:
+#             return {
+#                 'id': value.id,
+#                 'title': value.title,
+#                 'large': value.get_rendition('width-1000').attrs_dict,
+#                 'thumbnail': value.get_rendition('fill-128x128').attrs_dict,
+#                 'url': value.get_rendition('fill-128x128').url,
+#                 'alt': value.get_rendition('fill-128x128').alt
+#             }
 
 
 class NewProducts(Page):
     page_description = "Write about new products and display images here."
 
-    product_1 = StreamField([
-        ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
-        ('image', ImageChooserBlock('item_1_image')),
-        ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
-    ], use_json_field=True, blank=True, null=True)
-
-    product_2 = StreamField([
-        ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
-        ('image', ImageChooserBlock('item_2_image')),
-        ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
-    ], use_json_field=True, blank=True, null=True)
-
-    product_3 = StreamField([
-        ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
-        ('image', ImageChooserBlock('item_3_image')),
-        ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
-    ], use_json_field=True, blank=True, null=True)
-
-    product_4 = StreamField([
-        ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
-        ('image', ImageChooserBlock('item_4_image')),
-        ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
-    ], use_json_field=True, blank=True, null=True)
+    new_product = StreamField([
+        ('product', blocks.StructBlock([
+            ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
+            ('image', ImageChooserBlock(required=False, blank=True, null=True)),
+            ('url', blocks.URLBlock(required=False, blank=True, null=False)),
+        ])),
+    ], use_json_field=True, blank=True, null=False)
+    #
+    # product_2 = StreamField([
+    #     ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
+    #     ('image', ImageChooserBlock('item_2_image')),
+    #     ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
+    # ], use_json_field=True, blank=True, null=True)
+    #
+    # product_3 = StreamField([
+    #     ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
+    #     ('image', ImageChooserBlock('item_3_image')),
+    #     ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
+    # ], use_json_field=True, blank=True, null=True)
+    #
+    # product_4 = StreamField([
+    #     ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
+    #     ('image', ImageChooserBlock('item_4_image')),
+    #     ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
+    # ], use_json_field=True, blank=True, null=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('product_1'),
-        FieldPanel('product_2'),
-        FieldPanel('product_3'),
-        FieldPanel('product_4'),
+        FieldPanel('new_product'),
+        # FieldPanel('product_2'),
+        # FieldPanel('product_3'),
+        # FieldPanel('product_4'),
     ]
 
     api_fields = [
-        APIField('product_1'),
-        APIField('product_2'),
-        APIField('product_3'),
-        APIField('product_4'),
+        APIField('new_product'),
+        # APIField('product_2'),
+        # APIField('product_3'),
+        # APIField('product_4'),
     ]
