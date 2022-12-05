2b3f2de5829f1fcee96dea5376e36c31c7632c9b
Kyle <ksmontville@gmail.com>
2022-12-05 10:36:29 -0500

    Add StreamField for products page.

--- products/models.py
+++ products/models.py
@@ -23,66 +23,16 @@ class ImageChooserBlock(DefaultImageChooserBlock):
 class NewProducts(Page):
     page_description = "Write about new products and display images here."
 
-    item_1 = StreamField([
+    product = StreamField([
         ('description', blocks.RichTextBlock(features=ALL_FEATURES, required=False, blank=True, null=True)),
         ('image', ImageChooserBlock('item_1_image')),
         ('url', blocks.URLBlock(required=False, null=True, help_text="Link to store page here (optional)."))
     ], use_json_field=True, blank=True, null=True)
 
-    item_1_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-    item_2_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-    item_3_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-    item_4_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-
-    item_1_img = models.ForeignKey(
-        'wagtailimages.Image',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    item_2_img = models.ForeignKey(
-        'wagtailimages.Image',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    item_3_img = models.ForeignKey(
-        'wagtailimages.Image',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    item_4_img = models.ForeignKey(
-        'wagtailimages.Image',
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
     content_panels = Page.content_panels + [
-        FieldPanel('item_1'),
-        FieldPanel('item_1_text',),
-        FieldPanel('item_1_img'),
-        FieldPanel('item_2_text'),
-        FieldPanel('item_2_img'),
-        FieldPanel('item_3_text'),
-        FieldPanel('item_3_img'),
-        FieldPanel('item_4_text'),
-        FieldPanel('item_4_img'),
+        FieldPanel('product'),
     ]
 
     api_fields = [
-        APIField('item_1'),
-        APIField('item_1_text'),
-        APIField('item_1_image', serializer=ImageRenditionField('fill-128x128', source='item_1_img')),
-        APIField('item_2_text'),
-        APIField('item_2_image', serializer=ImageRenditionField('fill-128x128', source='item_2_img')),
-        APIField('item_3_text'),
-        APIField('item_3_image', serializer=ImageRenditionField('fill-128x128', source='item_3_img')),
-        APIField('item_4_text'),
-        APIField('item_4_image', serializer=ImageRenditionField('fill-128x128', source='item_4_img')),
+        APIField('product'),
     ]
