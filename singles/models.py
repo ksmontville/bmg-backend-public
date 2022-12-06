2c6728c8b2d8650b8ad305568505d02357a5ae23
Kyle <ksmontville@gmail.com>
2022-12-06 10:39:13 -0500

    Add logos to Singles page.

--- singles/models.py
+++ singles/models.py
@@ -14,8 +14,25 @@ class SinglesPage(HeadlessMixin, Page):
 
     header = RichTextField(features=ALL_FEATURES, blank=True, null=True)
     text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-    sub_text = RichTextField(features=ALL_FEATURES, blank=True, null=True)
-    logo = models.ForeignKey(
+    sub_text_one = RichTextField(features=ALL_FEATURES, blank=True, null=True)
+    sub_text_two = RichTextField(features=ALL_FEATURES, blank=True, null=True)
+    logo_1 = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    logo_2 = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    logo_3 = models.ForeignKey(
         'wagtailimages.Image',
         blank=True,
         null=True,
@@ -26,13 +43,19 @@ class SinglesPage(HeadlessMixin, Page):
     content_panels = Page.content_panels + [
         FieldPanel('header'),
         FieldPanel('text'),
-        FieldPanel('sub_text'),
-        FieldPanel('logo'),
+        FieldPanel('sub_text_one'),
+        FieldPanel('sub_text_two'),
+        FieldPanel('logo_1'),
+        FieldPanel('logo_2'),
+        FieldPanel('logo_3'),
     ]
 
     api_fields = [
         APIField('title'),
         APIField('text'),
-        APIField('sub_text'),
-        APIField('image', serializer=ImageRenditionField('fill-256x256', source='logo')),
+        APIField('sub_text_one'),
+        APIField('sub_text_two'),
+        APIField('image_1', serializer=ImageRenditionField('fill-128x128', source='logo_1')),
+        APIField('image_2', serializer=ImageRenditionField('fill-128x128', source='logo_2')),
+        APIField('image_3', serializer=ImageRenditionField('fill-128x128', source='logo_3')),
     ]
