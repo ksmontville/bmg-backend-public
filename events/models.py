a3961e42622adffccc30bdf4bbe07eac7a6f8f57
Kyle <ksmontville@gmail.com>
2022-12-09 12:48:54 -0500

    Integreate Google Calendar API functionality with Wagtail admin page.

--- events/models.py
+++ events/models.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
 
 from django.db import models
-from wagtail.admin.forms.models import WagtailAdminModelForm
 from wagtail.models import Page
-from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
+from wagtail.signals import page_published
 
 # Google functionality imports
 import datetime
@@ -20,34 +19,58 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 CALENDAR_ID = '3bfcac92ab15cf839d111cfa132668bb18c22473b8f19a318f5fb22a178c56a1@group.calendar.google.com'
 
+recurrence_choices = [
+    ('DAILY', 'DAILY'),
+    ('WEEKLY', 'WEEKLY'),
+    ('YEARLY', 'YEARLY')
+]
 
-class Event(models.Model):
-    title = models.CharField(max_length=50, null=True, blank=True)
+
+class Event(Page):
+    name = models.CharField(max_length=50, null=True, blank=True)
     location = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     start_date = models.DateField()
     start_time = models.TimeField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
-    recurrence = models.CharField(max_length=50, null=True, blank=True)
+    recurrence = models.CharField(max_length=50, choices=recurrence_choices, default='DAILY', null=True)
     google_link = models.CharField(max_length=150, null=True, blank=True)
     store_link = models.URLField(max_length=150, null=True, blank=True)
 
-    class Meta:
-        verbose_name_plural = 'Events'
-
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('location'),
+        FieldPanel('description'),
+        FieldPanel('start_date'),
+        FieldPanel('start_time'),
+        FieldPanel('end_date'),
+        FieldPanel('end_time'),
+        FieldPanel('recurrence'),
+        FieldPanel('google_link'),
+        FieldPanel('store_link'),
+    ]
 
     api_fields = [
+        APIField('name'),
+        APIField('location'),
+        APIField('description'),
+        APIField('start_date'),
+        APIField('start_time'),
+        APIField('end_date'),
+        APIField('end_time'),
+        APIField('recurrence'),
+        APIField('google_link'),
+        APIField('store_link'),
         APIField('store_link')
     ]
 
-    def save(self, **kwargs):
-        """
-        Custom save() method for overriding CharField behavior.
-        Google Calendar API requires UPPERCASE for recurrence field.
-        """
-        self.recurrence = self.recurrence.upper()
-        super(Event, self).save()
+    # def save(self, **kwargs):
+    #     """
+    #     Custom save() method for overriding CharField behavior.
+    #     """
+    #     self.create_event()
+    #     super(Event, self).save(**kwargs)
 
     def create_event(self):
         """Returns Google API OAuth2 credentials."""
@@ -95,3 +118,10 @@ class Event(models.Model):
             body=new_event).execute()
 
         print('Event created: %s' % (new_event.get(f'{self.store_link}')))
+
+
+def event_created(sender, instance, **kwargs):
+    instance.create_event()
+
+
+page_published.connect(event_created, sender=Event)
