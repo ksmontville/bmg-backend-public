d5c06d4cea49935ed67e39bb043c1ce2997bf4e2
Kyle <ksmontville@gmail.com>
2022-12-19 10:41:29 -0500

    Migrate to Postgres db.

--- blackmoongames/settings/base.py
+++ blackmoongames/settings/base.py
@@ -125,9 +125,13 @@ WSGI_APPLICATION = "blackmoongames.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd22epggssafos2',
+        'USER': 'klfcfedkduwedy',
+        'PASSWORD': '59156618dd0b6b5be2d3ec128a6ea5ff75093ed240234876c46ab3a67ade280b',
+        'HOST': 'ec2-34-231-63-30.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
 
